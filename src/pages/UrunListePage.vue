05a7dbe56309ef45982aa97a55b0081dca10ecf6
Melih ARIKAN <meliharikann@gmail.com>
2024-01-21 10:05:19 +0300

    Update UrunListePage.vue
    grid yapisi duzeltildi/daha sonra aslina uygun olarak style yapilacak

--- src/pages/UrunListePage.vue
+++ src/pages/UrunListePage.vue
@@ -1,9 +1,48 @@
 <template>
-  <UrunDetay/>
+  <div>
+    <h1>Ürünler</h1>
+    <div class="button-group">
+      <button @click="setGridColumn(2)">2 Kolon</button>
+      <button @click="setGridColumn(3)">3 Kolon</button>
+      <button @click="setGridColumn(4)">4 Kolon</button>
+    </div>
+    <button @click="fetchProducts">Verileri Çek</button>
+    <div class="urun-grid" :style="{ gridTemplateColumns: `repeat(${gridColumn}, 1fr)` }">
+      <TheUrun v-for="product in products" :key="product.urunKodu" :product="product" />
+    </div>
+  </div>
 </template>
+
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { useProductsStore } from 'stores/products';
+import TheUrun from 'src/components/TheUrun.vue';
 
-import UrunDetay from '/src/components/UrunDetay.vue';
+const productsStore = useProductsStore();
+const products = ref(productsStore.products);
+const gridColumn = ref<number>(3); // TS için tip belirtimi ekledik
 
+const setGridColumn = (columns: number) => {
+  gridColumn.value = columns;
+};
 
+const fetchProducts = async () => {
+  await productsStore.fetchProducts();
+  products.value = productsStore.products;
+};
+
+onMounted(() => {
+  fetchProducts();
+});
 </script>
+
+<style scoped>
+.urun-grid {
+  display: grid;
+  gap: 1px;
+}
+
+.button-group {
+  margin-bottom: 10px;
+}
+</style>
