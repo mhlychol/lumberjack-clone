7d11641e364a1abc25421213c0cf8ed9cb203f37
Melih ARIKAN <meliharikann@gmail.com>
2024-01-22 22:38:36 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
 <AnasayfaLayout/>
+
 </template>
 
 <script setup lang="ts">
 
 import AnasayfaLayout from './layouts/AnasayfaLayout.vue';
-import UrunListePage from './pages/UrunListePage.vue';
+//import UrunListePage from './pages/UrunListePage.vue';
 
 </script>
 
