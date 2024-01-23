bd1e6a75437425f4f6fcd4ba2c672282c6df0975
Melih ARIKAN <meliharikann@gmail.com>
2024-01-23 15:07:59 +0300

    Update UrunListePage.vue

--- src/pages/UrunListePage.vue
+++ src/pages/UrunListePage.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-
     <div class="button-group">
       <button @click="setGridColumn(2)">2 Kolon</button>
       <button @click="setGridColumn(3)">3 Kolon</button>
@@ -8,11 +7,9 @@
     </div>
     <div>
       <p>{{ $route.query.yas }}/{{ $route.query.cinsiyet }}/ {{ $route.query.ustTur }}/{{ $route.query.altTur }}</p>
-
     </div>
     <button @click="fetchProducts">Verileri Çek</button>
     <div class="urun-grid" :style="{ gridTemplateColumns: `repeat(${gridColumn}, 1fr)` }">
-      <!-- Use the filteredProducts array to display filtered products -->
       <TheUrun v-for="product in filteredProducts" :key="product.urunKodu" :product="product" />
     </div>
   </div>
@@ -26,64 +23,30 @@ import { useRoute } from 'vue-router';
 
 const productsStore = useProductsStore();
 const products = ref(productsStore.products);
-const gridColumn = ref<number>(3); // TS için tip belirtimi ekledik
-const filteredProducts = ref(productsStore.products); // Initialize with all products
+const gridColumn = ref<number>(3);
+const filteredProducts = ref(productsStore.products);
 
 const setGridColumn = (columns: number) => {
   gridColumn.value = columns;
+};
+
+const applyFilter = () => {
+  const { yas, cinsiyet, ustTur, altTur } = useRoute().query;
+  // Store'u kullanarak filtreleme işlemini gerçekleştir
+  filteredProducts.value = productsStore.applyFilter(yas, cinsiyet, ustTur, altTur);
+  fetchProducts();
+
 };
 
 const fetchProducts = async () => {
   await productsStore.fetchProducts();
   products.value = productsStore.products;
   // Update filteredProducts as well
-   // Reapply the filters after fetching new products
+  applyFilter(); // Reapply the filters after fetching new products
 };
 
-const applyFilter = () => {
-  const { yas, cinsiyet, ustTur, altTur } = useRoute().query;
-
-  // Tüm ürünlerle başla
-  let filteredProductsCopy = [...products.value];
-
-  // Filtreleri sırayla uygula
-  if (cinsiyet) {
-    if (cinsiyet === 'Erkek' || cinsiyet === 'Kadın') {
-      // Eğer cinsiyet "Erkek" veya "Kadın" ise, hem kendi cinsiyetini hem de "Unisex" olanları göster
-      filteredProductsCopy = filteredProductsCopy.filter(product =>
-        product['cinsiyet'] === cinsiyet || product['cinsiyet'] === 'Unisex'
-      );
-    } else {
-      // Diğer durumlar için normal filtreleme
-      filteredProductsCopy = filteredProductsCopy.filter(product => product['cinsiyet'] === cinsiyet);
-    }
-  }
-  if (yas) {
-    filteredProductsCopy = filteredProductsCopy.filter(product => product['yas'] == yas);
-  }
-  if (altTur) {
-    filteredProductsCopy = filteredProductsCopy.filter(product => product['altTur'] === altTur);
-  }
-  if (ustTur) {
-    filteredProductsCopy = filteredProductsCopy.filter(product => product['ustTur'] === ustTur);
-  }
-
-  // Eğer hiçbir filtre uymuyorsa veya filtre sonucunda hiç ürün yoksa, tüm ürünleri göster
-  if (!filteredProductsCopy.length) {
-    filteredProductsCopy = [...products.value];
-  }
-
-  // Sonuçla filteredProducts'ı güncelle
-  filteredProducts.value = filteredProductsCopy.length > 0 ? filteredProductsCopy : [];
-
-  console.log('Ürünler:', products.value);
-  console.log('Filtreler:', { cinsiyet, yas, altTur, ustTur });
-  console.log('Filtrelenmiş Ürünler:', filteredProductsCopy);
-};
 onMounted(() => {
-  applyFilter();
   fetchProducts();
-
 });
 </script>
 
