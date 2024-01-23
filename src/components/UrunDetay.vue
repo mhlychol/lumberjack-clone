f6e673e0f544f68a6790dfbb51626a9156a8c8b6
Melih ARIKAN <meliharikann@gmail.com>
2024-01-23 15:08:10 +0300

    Update UrunDetay.vue

--- src/components/UrunDetay.vue
+++ src/components/UrunDetay.vue
@@ -1,4 +1,6 @@
 <script lang="ts">
+import { ref } from 'vue';
+
 export default {
   props: {
     selectedProduct: {
@@ -6,60 +8,108 @@ export default {
       required: true,
     },
   },
+  setup() {
+    const lastSelectedRenkIndex = ref(0);
+    const selectedBedenIndex = ref(3);
+
+    const getStyleForEskiFiyat = (indirimOrani: number) => {
+      return indirimOrani !== 0
+        ? {
+          color: 'rgb(136, 136, 136)',
+          'text-decoration': 'line-through',
+        }
+        : {};
+    };
+    const getDeliveryDate = (): string => {
+      const currentDate = new Date();
+      const deliveryDate3 = new Date(currentDate);
+      deliveryDate3.setDate(currentDate.getDate() + 3);
+
+      const deliveryDate6 = new Date(currentDate);
+      deliveryDate6.setDate(currentDate.getDate() + 6);
+
+      const options: Intl.DateTimeFormatOptions = {
+        day: '2-digit',
+        month: 'long',
+      };
+
+      const formattedDate3 = deliveryDate3.toLocaleDateString('tr-TR', options);
+      const formattedDate6 = deliveryDate6.toLocaleDateString('tr-TR', options);
+
+      return `${formattedDate3.slice(0,-4)}- ${formattedDate6}`;
+    };
+
+    const getRenkImgUrl = (urunKodu: string, renk: string): string => {
+      return `src/assets/${urunKodu}${renk}1.webp`;
+    };
+
+    const getImgUrl = (urunKodu: string, renk: string, index: number): string => {
+      return `src/assets/${urunKodu}${renk}${index}.webp`;
+    };
+    const handleMouseOver = (index: number): void => {
+      lastSelectedRenkIndex.value = index;
+    };
+    const handleMouseOverbeden = (index: number): void => {
+      selectedBedenIndex.value = index;
+    };
+
+    return {
+      lastSelectedRenkIndex,
+      selectedBedenIndex,
+      getStyleForEskiFiyat,
+      getRenkImgUrl,
+      handleMouseOver,
+      handleMouseOverbeden,
+      getImgUrl,
+      getDeliveryDate,
+    };
+  },
 };
 </script>
 
 <template>
-   <div v-if="selectedProduct">
-      <h3>{{ selectedProduct.urunKodu }}</h3>
-      <p>marka: {{ selectedProduct.marka }}</p>
-      <p>Model: {{ selectedProduct.model }}</p>
-      <p>urunAciklamasi: {{ selectedProduct.urunAciklamasi }}</p>
-    </div>
+  <div v-if="selectedProduct">
+    <h3>{{ selectedProduct.urunKodu }}</h3>
+    <p>marka: {{ selectedProduct.marka }}</p>
+    <p>Model: {{ selectedProduct.model }}</p>
+    <p>urunAciklamasi: {{ selectedProduct.urunAciklamasi }}</p>
+  </div>
   <div class="Urundetayconteiner">
     <div class="Urunconteiner">
-      <div class="resimler">
-        <div class="item">
-          <img class="itemimg" src="/src/assets/griesofman1.webp" alt="">
+      <div class="resimler" id="app">
+        <div class="item" v-for="index in 5" :key="index">
+          <img class="itemimg"
+            :src="getImgUrl(selectedProduct.urunKodu, selectedProduct.renk[lastSelectedRenkIndex], index)" alt="">
         </div>
-        <div class="item">
-          <img class="itemimg" src="/src/assets/griesofman2.webp" alt="">
-        </div>
-        <div class="item">
-          <img class="itemimg" src="/src/assets/griesofman3.webp" alt="">
-        </div>
-        <div class="item">
-          <img class="itemimg" src="src/assets/griesofman4.webp" alt="">
-        </div>
-        <div class="item" />
-        <div class="item" />
       </div>
       <div class="Urundetaybilgiler">
         <div class="model">
-
+          {{ selectedProduct.urunAciklamasi }}
         </div>
         <div class="Urundetaykargoindirimdurum">
-          <div class="Urundetayindirimdurum">
-            -%
+          <div class="Urundetayindirimdurum" v-if="selectedProduct.indirimOrani > 0">
+            -%{{ selectedProduct.indirimOrani }}
           </div>
-          <div class="Urundetaykargodurum">
+          <div class="Urundetaykargodurum" v-if="selectedProduct.kargo === 'Ücretsiz'">
             Kargo Bedava
           </div>
         </div>
         <div class="Urundetaykuponsezon">
           <div class="Urundetaykupon">
-            <img src="assets/kupon.webp" alt="">
+            <img src="src/assets/kuponindirimbilgi1.webp" alt="">
           </div>
           <div class="Urundetaysezon">
-            <img src="assets/yeni-sezon.webp" alt="">
+            <img src="src/assets/kuponindirimbilgi2.webp" alt="">
+          </div>
+          <div class="Urundetaysezon">
+            <img src="src/assets/kuponindirimbilgi3.webp" alt="">
           </div>
         </div>
         <div class="Urundetayeskisonfiyat">
-          <div class="Urundetayeskifiyat">
-            449,99 TL
-          </div>
-          <div class="Urundetaysonfiyat">
-            359,99 TL
+          <div class="Urundetayeskifiyat" :style="getStyleForEskiFiyat(selectedProduct.indirimOrani)">
+            {{ selectedProduct.fiyat }} TL </div>
+          <div class="Urundetaysonfiyat" v-if="selectedProduct.indirimOrani > 0">
+            {{ (selectedProduct.fiyat * ((100 - selectedProduct.indirimOrani) / 100)).toFixed(2) }} TL
           </div>
         </div>
         <div class="renk">
@@ -67,27 +117,17 @@ export default {
             Renk:
           </div>
           <div class="renkrenk">
-            Gri
+            {{ selectedProduct.renk[lastSelectedRenkIndex] }}
           </div>
         </div>
         <div class="Urundetayrenkresimler">
-          <div class="Urundetayrenkresimconteiner">
-            <Button class="Urundetayrenkresim">
-              <img class="itemimg" src="src/assets/griesofman1.webp" alt="">
+
+          <div v-for="(renk, index) in selectedProduct.renk" :key="index" class="Urundetayrenkresimconteiner">
+            <Button class="Urundetayrenkresim" @click="handleMouseOver(index)">
+              <img class="itemimg" :src="getRenkImgUrl(selectedProduct.urunKodu, renk)" alt="">
             </Button>
-            <div class="Urundetayseciliitem" />
-          </div>
-          <div class="Urundetayrenkresimconteiner">
-            <Button class="Urundetayrenkresim">
-              <img class="itemimg" src="src/assets/siyahesofman3.webp" alt="">
-            </Button>
-            <div class="Urundetayseciliitem" style="visibility: hidden;" />
-          </div>
-          <div class="Urundetayrenkresimconteiner">
-            <Button class="Urundetayrenkresim">
-              <img class="itemimg" src="src/assets/bejesofman1.webp" alt="">
-            </Button>
-            <div class="Urundetayseciliitem" style="visibility: hidden;" />
+            <div class="Urundetayseciliitem"
+              :style="{ visibility: index === lastSelectedRenkIndex ? 'visible' : 'hidden' }" />
           </div>
         </div>
 
@@ -101,27 +141,17 @@ export default {
             </button>
           </div>
           <div class="Urundetaybedecesit">
-            <div class="Urundetaybeden">
-              S
-            </div>
-            <div class="Urundetaybeden" style="border-color: black; color: black;">
-              M
-            </div>
-            <div class="Urundetaybeden">
-              L
-            </div>
-            <div class="Urundetaybeden">
-              XL
-            </div>
-            <div class="Urundetaybeden">
-              2XL
+            <div v-for="(beden, index) in selectedProduct.bedenler" :key="index" class="Urundetaybeden" @click="handleMouseOverbeden(index)"
+              :style="{ backgroundColor: index === selectedBedenIndex ? 'black' : '', color: index === selectedBedenIndex ? 'white' : '' }">
+              {{ beden }}
             </div>
           </div>
+
 
           <div class="Urundetaykargosure">
             <i class="fa-solid fa-truck" />
             <div class="Urundetaykargosureyazi">
-              Tahmini Kargoya Teslimat : 13 - 14 Aralık
+              Tahmini Kargoya Teslimat : {{ getDeliveryDate() }}
             </div>
           </div>
           <button class="Urundetaysepetekle">
@@ -141,250 +171,274 @@ export default {
 
 <style>
 .Urundetayconteiner {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
 }
 
 .Urunconteiner {
-    display: flex;
-    flex-direction: row;
-    width: 100%;
-    padding: 0 15px 0 15px;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  padding: 0 15px 0 15px;
 
 }
 
 .resimler {
-    width: 66%;
-    height: auto;
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    /* 3 sütun */
-    grid-gap: 10px;
-    /* Sütun ve satırlar arası boşluk */
-    padding: 0 15px 0 15px;
+  width: 66%;
+  height: auto;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 10px;
+  padding: 0 15px 0 15px;
   max-width: 66%;
 }
 
 .item {
-    color: #fff;
-    text-align: center;
+  color: #fff;
+  text-align: center;
 }
-.itemimg{
-  max-width: 100%;
+
+.itemimg {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
+
 .Urundetaybilgiler {
-    display: flex;
-    flex-direction: column;
-    width: 33%;
-    padding: 0 0 0 15px;
+  display: flex;
+  flex-direction: column;
+  width: 33%;
+  padding: 0 0 0 15px;
 
 }
 
 .model {
-    font-family: sans-serif;
-    font-size: 16px;
-    margin: 0 0 5px 0;
+  font-family: sans-serif;
+  font-size: 16px;
+  margin: 0 0 5px 0;
 
 }
 
 .Urundetaykargoindirimdurum {
-    display: flex;
-    flex-direction: row;
-    margin: 5px 0 5px 0;
-
+  display: flex;
+  flex-direction: row;
+  margin: 6px 0 5px 0;
 }
 
 .Urundetayindirimdurum {
-    color: white;
-    background-color: black;
-    height: 25px;
-    margin-right: 5px;
-    padding: 0 5px 0 5px;
-    justify-content: center;
-    align-items: center;
-    font-weight: 600;
-    font-size: 16px;
+  color: white;
+  background-color: black;
+  height: 25px;
+  margin-right: 8px;
+  padding: 0 5px 0 5px;
+  justify-content: center;
+  align-items: center;
+  font-weight: 400;
+  font-size: 15px;
 }
 
 .Urundetaykargodurum {
-    color: rgb(0, 0, 0);
-    background-color: rgb(232, 232, 232);
-    height: 25px;
-    padding: 0 5px 0 5px;
-    align-items: center;
+  color: rgb(0, 0, 0);
+  background-color: rgb(232, 232, 232);
+  height: 18px;
+  padding: 0 5px 0 5px;
+  align-items: center;
+  font-size: 12px;
+  margin-top: 5px;
+
 
 }
 
 .Urundetaykuponsezon {
-    display: flex;
-    flex-direction: row;
-    margin: 5px 0 5px 0;
+  display: flex;
+  flex-direction: row;
+  margin: 5px 0 5px 0;
 
 }
 
 .Urundetaykupon img {
-    height: 35px;
+  height: 35px;
 
 }
 
 .Urundetaysezon img {
-    height: 35px;
-    padding-left: 5px;
+  height: 35px;
+  padding-left: 5px;
 
 }
 
 .Urundetayeskisonfiyat {
-    display: flex;
-    flex-direction: row;
-    font-size: 16px;
-    font-weight: 400;
+  display: flex;
+  flex-direction: row;
+  font-size: 16px;
+  font-weight: 400;
 
 }
 
 .Urundetayeskifiyat {
-    color: rgb(136, 136, 136);
-    text-decoration: line-through;
-    padding-right: 10px;
+  color: rgb(0, 0, 0);
+  padding-right: 10px;
 }
 
 .Urundetaysonfiyat {
-    color: red;
+  color: red;
 
 }
 
 .renk {
-    display: flex;
-    flex-direction: row;
-    font-size: 16px;
-    padding-top: 10px;
+  display: flex;
+  flex-direction: row;
+  font-size: 16px;
+  padding: 10px 0 10px 0;
 
 }
 
 .renkyazi {
-    color: rgb(136, 136, 136);
-    padding-right: 5px;
+  color: rgb(136, 136, 136);
+  padding-right: 5px;
 }
 
 .renkrenk {
-    color: black;
-    font-weight: 700;
+  color: black;
+  font-weight: 600;
+  font-size: 16px;
+  font-family: Helvetica;
 }
 
 .Urundetayrenkresimler {
-    display: flex;
-    flex-direction: row;
-    margin: 5px 15px 5px 0;
-}
-
-.Urundetayrenkresimconteiner {
-    width: 45px;
-    height: 100px;
-    margin-right: 5px;
+  display: flex;
+  flex-direction: row;
+  padding: 0;
+  margin: 0;
 }
 
 .Urundetayrenkresim {
-    height: 60px;
-    width: 45px;
+  height: 60px;
+  width: 45px;
+  padding: 0;
+  margin: 0;
+  border: none;
+  background-color: transparent;
+}
+
+.Urundetayrenkresimconteiner {
+  width: 45px;
+  height: 100px;
+  margin-right: 5px;
+}
+
+.Urundetayrenkresim {
+  height: 60px;
+  width: 45px;
+  padding: 0;
+  margin: 0;
 }
 
 .Urundetayseciliitem {
-    height: 3px;
-    background-color: black;
-    margin: 0px 0 15px 0;
+  height: 3px;
+  background-color: black;
+  margin: 0px 0 15px 0;
 
 }
 
 .Urundetayalt {
-    display: flex;
-    flex-direction: column;
-    width: 66%;
+  display: flex;
+  flex-direction: column;
+  width: 66%;
 }
 
 .Urundetaybedenyazi {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 }
 
 .Urundetayyazi {
-    color: rgb(136, 136, 136);
+  color: rgb(136, 136, 136);
 
 }
 
 .Urundetaytablo {
-    text-decoration: underline;
-    color: rgb(0, 0, 0);
-
-    font-size: 12px;
-
+  text-decoration: underline;
+  color: rgb(0, 0, 0);
+  border: none;
+  font-size: 12px;
+  background-color: transparent;
 }
 
 .Urundetaybedecesit {
-    display: flex;
-    flex-direction: row;
+  display: flex;
+  flex-direction: row;
 }
 
 .Urundetaybeden {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 40px;
-    width: 40px;
-    border: solid 1px rgb(136, 136, 136);
-    margin: 5px;
-    color: rgb(136, 136, 136);
-}
-.Urundetaykargosure{
-    display: flex;
-    flex-direction: row;
-    background-color: rgb(236, 236, 236);
-    height: 30px;
-    padding: 0 0 0 10px;
-    align-items: center;
-    margin: 20px 0 5px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 40px;
+  width: 43px;
+  border: solid 1px rgb(136, 136, 136);
+  margin: 3px;
+  color: rgb(136, 136, 136);
+  font-size: 11px;
 
 }
+
+.Urundetaykargosure {
+  display: flex;
+  flex-direction: row;
+  background-color: rgb(236, 236, 236);
+  height: 30px;
+  padding: 0 0 0 10px;
+  align-items: center;
+  margin: 20px 0 5px 0;
+
+}
+
 .Urundetaykargosure i {
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 }
 
-.Urundetaykargosureyazi  {
-    display: flex;
-    align-items: center;
-    text-align: center;
-    font-size: 10px;
-    padding-left: 15px;
-    letter-spacing: 1px;
+.Urundetaykargosureyazi {
+  display: flex;
+  align-items: center;
+  text-align: center;
+  font-size: 10px;
+  padding-left: 15px;
+  letter-spacing: 1px;
 }
-.Urundetaysepetekle{
-    display: flex;
-    align-items: center;
-    background-color: black;
-    color: white;
-    margin: 10px 0 10px 0;
-    height: 40px;
-    font-size:16px;
-    font-weight: 400;
-    letter-spacing: 1px;
-    text-align: center;
-    justify-content: center;
+
+.Urundetaysepetekle {
+  display: flex;
+  align-items: center;
+  background-color: black;
+  color: white;
+  margin: 10px 0 10px 0;
+  height: 40px;
+  font-size: 16px;
+  font-weight: 400;
+  letter-spacing: 1px;
+  text-align: center;
+  justify-content: center;
 }
-.Urundetayfavekle{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
+
+.Urundetayfavekle {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 
 }
-.Urundetayfavekleyazi{
-    display: flex;
-    align-items: center;
-    text-align: center;
-    font-size: 16px;
-    padding-left: 5px;
-    letter-spacing: 1px;
-    font-weight: 400;
+
+.Urundetayfavekleyazi {
+  display: flex;
+  align-items: center;
+  text-align: center;
+  font-size: 16px;
+  padding-left: 5px;
+  letter-spacing: 1px;
+  font-weight: 400;
 }
 </style>
