072092b1366b21f422e804ac11cd03efee2bd4cd
Melih ARIKAN <meliharikann@gmail.com>
2024-01-29 04:08:32 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
-<SepetFirebaseDeneme/>
+<router-view to="/"/>
 </template>
 
 <script setup lang="ts">
 import { onMounted } from 'vue';
+import { useSepetStore } from 'stores/sepet';
+import { useProductStore } from 'stores/products';
+
+const productStore = useProductStore();
+const sepetStore = useSepetStore();
+
 
 import AnasayfaLayout from './layouts/AnasayfaLayout.vue';
 import DenemePage from './pages/DenemePage.vue';
 import SepetFirebaseDeneme from './pages/SepetFirebaseDeneme.vue';
+import TheSepet from './components/TheSepet.vue';
 //import UrunListePage from './pages/UrunListePage.vue';
+import SepetDetay from './components/SepetDetay.vue';
 
 
 
 onMounted(() => {
-  //productsStore.fetchProducts();
+  productStore.fetchProducts();
+  sepetStore.getFirebaseToSepet();
 });
 
 </script>
