fe4fe1dac612029d1e9229e1c4c0167a044e99c0
Melih ARIKAN <meliharikann@gmail.com>
2024-01-29 04:08:06 +0300

    Update TheSepet.vue

--- src/components/TheSepet.vue
+++ src/components/TheSepet.vue
@@ -1,31 +1,72 @@
-<script>
-import SepetUrun from 'src/components/SepetUrun.vue' // Adjust the path accordingly
+<script lang="ts">
+import SepetUrun from 'src/components/SepetUrun.vue'; // Adjust the path accordingly
+import { useSepetStore } from 'stores/sepet';
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
 
 export default {
   components: {
     SepetUrun,
   },
+
+  setup() {
+    const sepetStore = useSepetStore();
+    const sepetUrunler = sepetStore.sepetUrunleri;
+    const router = useRouter();
+
+    onMounted(() => {
+      sepetStore.getFirebaseToSepet();
+    });
+
+    console.log('sepeturunleri', sepetUrunler.values); // Log the data to the console
+
+    const toplamAdetHesapla = () => {
+      const toplam = sepetUrunler.reduce((acc, urun) => acc + urun.adet, 0);
+      const basamaklar = toplam.toString().split('').map(Number);
+      const toplamson = basamaklar.reduce((acc, curr) => acc + curr, 0);
+      return toplamson;
+    };
+
+    const toplamfiyatHesapla = () => {
+      const toplamson = sepetUrunler.reduce((acc, urun) => acc + (urun.fiyat * urun.adet), 0);
+      return toplamson;
+    };
+
+    const redirectToSiparis = () => {
+      // Programatik olarak yönlendirme
+      router.push('/sepet/');
+    };
+
+    return {
+      sepetUrunler,
+      toplamson: toplamAdetHesapla(),
+      totalfiyat: toplamfiyatHesapla(),
+      redirectToSiparis,
+    };
+  },
+
+
   // other component options...
 }
 </script>
 
 <template>
   <div class="sepetmainconteiner">
+
     <div class="urunsayisi">
-      1 Adet ürün sepetinizde ekli
+      {{ toplamson }} Adet ürün sepetinizde ekli
     </div>
     <div class="aratoplam">
-      Ara Toplam: 699,99 TL
+      Ara Toplam:{{ totalfiyat }} TL
     </div>
     <div class="sepetbutonconteiner">
-      <button class="sepetbuton">
-        SEPETE GİT
+      <button class="sepetbuton" @click="redirectToSiparis">
+          SEPETE GİT
       </button>
     </div>
     <div style="background-color: gray; height: 1px;" />
-    <div class="urunler">
-      <SepetUrun />
-      <SepetUrun />
+    <div>
+      <SepetUrun v-for="sepeturun in sepetUrunler" :key="sepeturun.urunKodu" :urun="sepeturun" />
     </div>
     <div style="background-color: gray; height: 1px;" />
   </div>
@@ -64,11 +105,12 @@ export default {
   height: 30px;
   font-size: 14px;
 }
-.urunler{
-    display: flex;
+
+.urunler {
+  display: flex;
   flex-direction: column;
-  padding: 5px 0 5px 0 ;
+  padding: 5px 0 5px 0;
   max-height: 300px;
-    overflow: scroll;
+  overflow: scroll;
 }
 </style>
