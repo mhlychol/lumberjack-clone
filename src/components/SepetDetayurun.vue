646aca91b84bb915c030f521f8d7b6bbe55ebcd7
Melih ARIKAN <meliharikann@gmail.com>
2024-02-02 04:07:40 +0300

    Update SepetDetayurun.vue

--- src/components/SepetDetayurun.vue
+++ src/components/SepetDetayurun.vue
@@ -1,23 +1,81 @@
 <script lang="ts">
+import { useSepetStore } from 'stores/sepet';
+import { ref } from 'vue';
+const sepetStore = useSepetStore();
+const sepetUrunler = sepetStore.sepetUrunleri;
 export default {
 
- props: {
+  props: {
     urun: {
       type: Object,
       required: true,
     },
   },
-}
-const calculateIndirimliFiyat = (fiyat, indirimOrani) => {
-  // İndirim varsa yeni fiyatı hesapla
-  if (indirimOrani !== 0) {
-    const indirimliFiyat = fiyat - (fiyat * indirimOrani) / 100;
-    return indirimliFiyat.toFixed(2); // İki ondalık basamaklı olarak göster
-  }
-  // İndirim yoksa mevcut fiyatı geri döndür
-  return fiyat;
-};
-const toplamAdetHesapla = () => {
+  data() {
+    return {
+      productCount: ref(this.urun.adet),
+    };
+  },
+  methods: {
+    urunsartirazalt(operator, urun) {
+      if (operator === '+') {
+        this.productCount++;
+      } else if (operator === '-' && this.productCount > 0) {
+        this.productCount--;
+      }
+      // sepete ekleme cikarma yapcm burda
+      urun.adet = this.productCount;
+      console.log(urun.adet)
+    },
+    callUrunSilme(urunkodu: string, renk: string, beden: string, adet: number) {
+
+      sepetStore.urunSil({ urunKodu: urunkodu, renk: renk, beden: beden, adet: adet });
+    }
+  },
+  setup() {
+    const getDeliveryDate = (): string => {
+      const currentDate = new Date();
+      const deliveryDate3 = new Date(currentDate);
+      deliveryDate3.setDate(currentDate.getDate() + 1);
+
+      const deliveryDate6 = new Date(currentDate);
+      deliveryDate6.setDate(currentDate.getDate() + 2);
+
+      const options: Intl.DateTimeFormatOptions = {
+        day: '2-digit',
+        month: 'long',
+      };
+
+      const formattedDate3 = deliveryDate3.toLocaleDateString('tr-TR', options);
+      const formattedDate6 = deliveryDate6.toLocaleDateString('tr-TR', options);
+
+      return `${formattedDate3.slice(0, 2)} - ${formattedDate6}`;
+    };
+
+
+    const getStyleForEskiFiyat = (indirimOrani) => {
+      return indirimOrani !== 0
+        ? {
+          color: 'rgb(136, 136, 136)',
+          'text-decoration': 'line-through',
+          'font-size': '12',
+          'font-weight': '400',
+        }
+        : {};
+
+    };
+
+
+    const calculateIndirimliFiyat = (fiyat, indirimOrani) => {
+      // İndirim varsa yeni fiyatı hesapla
+      if (indirimOrani !== 0) {
+        const indirimliFiyat = fiyat - (fiyat * indirimOrani) / 100;
+        return indirimliFiyat.toFixed(2); // İki ondalık basamaklı olarak göster
+      }
+      // İndirim yoksa mevcut fiyatı geri döndür
+      return fiyat;
+    };
+    const toplamAdetHesapla = () => {
       const toplam = sepetUrunler.reduce((acc, urun) => acc + urun.adet, 0);
       const basamaklar = toplam.toString().split('').map(Number);
       const toplamson = basamaklar.reduce((acc, curr) => acc + curr, 0);
@@ -28,6 +86,19 @@ const toplamAdetHesapla = () => {
       const toplamson = sepetUrunler.reduce((acc, urun) => acc + (urun.fiyat * urun.adet), 0);
       return toplamson;
     };
+
+    return {
+      sepetUrunler,
+      toplamson: toplamAdetHesapla(),
+      totalfiyat: toplamfiyatHesapla(),
+      calculateIndirimliFiyat,
+      getDeliveryDate,
+      getStyleForEskiFiyat,
+    };
+
+  }
+
+}
 </script>
 <template>
   <div class="siparisozetconteiner">
@@ -48,7 +119,7 @@ const toplamAdetHesapla = () => {
               Tahmini Kargoya Teslimat :
             </div>
             <div class="urunkargosuresag">
-              7 - 8 Aralık
+              {{ getDeliveryDate() }}
             </div>
           </div>
         </div>
@@ -58,28 +129,31 @@ const toplamAdetHesapla = () => {
           Beden:
         </div>
         <div class="bedensag">
-          {{urun.beden}}
+          {{ urun.beden }}
         </div>
       </div>
       <div class="urunsayiconteiner">
         <div class="urunsayicerceve">
+          <div class="urunsayibuton">
+            <button class="butonstyleadet" @click="urunsartirazalt('-', urun)" v-show="productCount > 1">-</button>
+          </div>
           <div class="urunsayi">
-            {{urun.adet}}
+            {{ productCount }}
           </div>
           <div class="urunsayibuton">
-            <button class="butonstyle">+</button>
+            <button class="butonstyleadet" @click="urunsartirazalt('+', urun)">+</button>
           </div>
         </div>
       </div>
       <div class="urunfiyat">
-        <div class="urunfiyateski">
+        <div class="urunfiyateski" :style="getStyleForEskiFiyat(urun.indirimOrani)">
           {{ urun.fiyat }}TL
         </div>
         <div class="urunfiyatyeni" v-if="urun.indirimOrani > 0">
           {{ calculateIndirimliFiyat(urun.fiyat, urun.indirimOrani) }}TL
         </div>
       </div>
-      <div class="silbutonconteiner">
+      <div class="silbutonconteiner" @click="callUrunSilme(urun.urunKodu, urun.renk, urun.beden, urun.adet)">
         <div class="silbuton">
           <button class="silbutonbuton">
             Sil <i class="fa-solid fa-trash-can" style="color: #bababa;" />
@@ -90,193 +164,208 @@ const toplamAdetHesapla = () => {
   </div>
 </template>
 
-  <style>
-  .butonstyle {
-border: none;
-background-color: transparent;
-  }
-  .silbutonbuton {
-border: none;
-background-color: transparent;
-  }
-  .siparisozetconteiner {
+<style>
+.butonstyleadet {
+  border: none;
+  background-color: transparent;
+}
 
-      display: flex;
-      flex-direction: column;
-      width: auto;
-  }
+.silbutonbuton:hover,
+.butonstyleadet:hover {
+  cursor: pointer;
+}
 
-  .siparisozet {
-      display: flex;
-      flex-direction: row;
-      height: 120px;
-      background-color: rgb(255, 255, 255);
-      justify-content: space-between;
-      align-items: center;
-      padding:10px;
-  }
+.silbutonbuton {
+  border: none;
+  background-color: transparent;
+}
 
-  .urunozet {
-      display: flex;
-      flex-direction: row;
-      width: 40%;
-      height: 100%;
-      align-items: center;
-  }
+.siparisozetconteiner {
 
-  .urunresmi {
-      width: 20%;
-      height: 100%;
-  }
+  display: flex;
+  flex-direction: column;
+  width: auto;
+}
 
-  .urunresmi img {
-      display: block;
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-      border-radius: 5px;
+.siparisozet {
+  display: flex;
+  flex-direction: row;
+  height: 120px;
+  background-color: rgb(255, 255, 255);
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+}
 
-  }
+.urunozet {
+  display: flex;
+  flex-direction: row;
+  width: 50%;
+  height: 100%;
+  align-items: center;
+}
 
-  .urunbilgi {
-      display: flex;
-      flex-direction: column;
-      width: 75%;
-      height: 60%;
-      align-items: center;
-      padding-left: 10px;
+.urunresmi {
+  width: 20%;
+  height: 100%;
+}
 
-  }
+.urunresmi img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 5px;
 
-  .urunmarka {
-      display: flex;
-      height: 33%;
-      width: 100%;
-      margin-bottom: 2px;
-      font-weight: 800;
-  }
+}
 
-  .urunyazi {
-      display: flex;
-      height: 33%;
-      width: 100%;
-      overflow: hidden;
-      font-size: 12px;
-      text-decoration-color: black;
-      color: rgb(167, 167, 167);
-  }
+.urunbilgi {
+  display: flex;
+  flex-direction: column;
+  width: 75%;
+  height: 60%;
+  align-items: center;
+  padding-left: 10px;
 
-  .urunyazi a {
-      color: rgb(167, 167, 167);
-  }
+}
 
-  .urunkargosure {
-      display: flex;
-      height: 33%;
-      width: 100%;
-      font-size: 12px;
-      flex-direction: column;
-  }
+.urunmarka {
+  display: flex;
+  height: 33%;
+  width: 100%;
+  margin-bottom: 2px;
+  font-weight: 800;
+}
 
-  .urunkargosuresol {
-      display: flex;
-      width: 70%;
-      color: green;
-      letter-spacing: 1px;
-  }
+.urunyazi {
+  display: flex;
+  height: 33%;
+  width: 100%;
+  overflow: hidden;
+  font-size: 12px;
+  text-decoration-color: black;
+  color: rgb(167, 167, 167);
+}
 
-  .urunkargosuresag {
-      display: flex;
-      width: 30%;
-  }
+.urunyazi a {
+  color: rgb(167, 167, 167);
+}
 
-  .beden {
-      display: flex;
-      flex-direction: row;
-      width: 20%;
-      height: 20px;
-      justify-content: center;
-      align-items: center;
-      font-size: 13px;
-  }
+.urunkargosure {
+  display: flex;
+  height: 33%;
+  width: 100%;
+  font-size: 12px;
+  flex-direction: row;
+  align-items: center;
+}
 
-  .bedensol {
-      color: rgb(167, 167, 167);
-      font-weight: 500;
-      text-align: justify;
-      transform: scaleX(1.15);
-      margin-right: 5px;
-  }
+.urunkargosuresol {
+  display: flex;
+  width: 70%;
+  color: green;
+  letter-spacing: 1px;
+}
 
-  .bedensag {
-      font-weight: 400;
-  }
+.urunkargosuresag {
+  display: flex;
+  width: 40%;
+  color: rgb(110, 110, 110);
+  font-size: 13px;
+  font-family: sans-serif;
+  font-weight: 500;
+}
 
-  .urunsayiconteiner {
-      display: flex;
-      flex-direction: row;
-      width: 20%;
-      height: 50px;
-      align-items: center;
-      padding-left: 25px;
+.beden {
+  display: flex;
+  flex-direction: row;
+  width: 20%;
+  height: 20px;
+  justify-content: center;
+  align-items: center;
+  font-size: 13px;
+}
 
-  }
+.bedensol {
+  color: rgb(167, 167, 167);
+  font-weight: 500;
+  text-align: justify;
+  transform: scaleX(1.15);
+  margin-right: 5px;
+}
 
-  .urunsayicerceve {
-      display: flex;
-      flex-direction: row;
-      height: 25px;
-      width: 75px;
-      border: 1px solid rgb(167, 167, 167);
-      border-radius: 25px;
-      font-size: 16px;
-      text-align: center;
-      justify-content: center;
-      align-items: center
+.bedensag {
+  font-weight: 400;
+}
+
+.urunsayiconteiner {
+  display: flex;
+  flex-direction: row;
+  width: 20%;
+  height: 50px;
+  align-items: center;
+  padding-left: 25px;
+
+}
+
+.urunsayicerceve {
+  display: flex;
+  flex-direction: row;
+  height: 30px;
+  width: 85px;
+  border: 1px solid rgb(206, 206, 206);
+  font-size: 16px;
+  text-align: center;
+  justify-content: space-evenly;
+  align-items: center;
 
 
-  }
 
-  .urunsayi {
-      font-style: sans-serif;
-  }
+}
 
-  .urunsayibuton {
-      position: absolute;
-      float: right;
-      margin-left: 50px;
-      font-size: 18px;
-      justify-content: center;
-  }
+.urunsayi {
+  font-style: sans-serif;
+  width: 20px;
+}
 
-  .urunfiyat {
-      display: flex;
-      flex-direction: column;
-      width: 15%;
-      height: 80px;
-      justify-content: center;
-      align-items: center;
-  }
+.urunsayibuton {
+  float: right;
+  font-size: 18px;
+  justify-content: center;
+  width: 20px;
+}
 
-  .urunfiyateski {
-      color: rgb(145, 145, 145);
-      text-decoration: line-through;
+.urunfiyat {
+  display: flex;
+  flex-direction: column;
+  width: 15%;
+  height: 80px;
+  justify-content: center;
+  align-items: center;
+  font-size: 16px;
+  text-align: justify;
 
-  }
+}
 
-  .urunfiyatyeni {
-      font-size: 15px;
-      font-weight: 800;
-  }
+.urunfiyateski {
+  color: rgb(0, 0, 0);
+  font-weight: 700;
 
-  .silbutonconteiner {
-      display: flex;
-      flex-direction: row;
-      width: 5%;
-      height: 50px;
-      justify-content: center;
-      align-items: center;
-      color: rgb(167, 167, 167);
-      padding-left: 2px;
-  }
-  </style>
+}
+
+.urunfiyatyeni {
+  font-size: 15px;
+  font-weight: 700;
+}
+
+.silbutonconteiner {
+  display: flex;
+  flex-direction: row;
+  width: 8%;
+  height: 50px;
+  justify-content: center;
+  align-items: center;
+  color: rgb(167, 167, 167);
+  padding-left: 2px;
+}
+</style>
