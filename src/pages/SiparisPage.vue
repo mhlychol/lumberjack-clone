a2d76472465cd549b5a43d56df48b2f1a98f79e7
Melih ARIKAN <meliharikann@gmail.com>
2024-02-02 04:08:05 +0300

    Create SiparisPage.vue

---
+++ src/pages/SiparisPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- Your other template content here -->
+    <SepetDetay></SepetDetay>
+  </div>
+</template>
+
+<script lang="ts">
+import SepetDetay from 'components/SepetDetay.vue';
+
+export default {
+  components: {
+    SepetDetay,
+  },
+  // Your other script content here
+};
+</script>
