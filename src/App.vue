a91b15448b5ed029f17e54c5fb27a75c3100e02d
Melih ARIKAN <meliharikann@gmail.com>
2024-02-02 16:10:25 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
-<router-view to="/"/>
+  <router-view to="/" />
 </template>
 
 <script setup lang="ts">
 import { onMounted } from 'vue';
 import { useSepetStore } from 'stores/sepet';
 import { useProductStore } from 'stores/products';
-
 const productStore = useProductStore();
 const sepetStore = useSepetStore();
 
