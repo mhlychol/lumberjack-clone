6adab87afa34b1e1fe95e5ccfd979a1202a602f0
Melih ARIKAN <meliharikann@gmail.com>
2024-02-02 04:07:51 +0300

    Update sepet.ts

--- src/stores/sepet.ts
+++ src/stores/sepet.ts
@@ -7,93 +7,98 @@ interface Sepeturun {
   urunAciklamasi: string;
   fiyat: number;
   indirimOrani: number;
-   renk: string;
-   beden: string;
-   adet: number;
-  }
+  renk: string;
+  beden: string;
+  adet: number;
+}
 
 
 export const useSepetStore = defineStore(
-   {id:'sepet',
-  state: () => ({
-    sepetUrunleri: [] as Sepeturun [],
+  {
+    id: 'sepet',
+    state: () => ({
+      sepetUrunleri: [] as Sepeturun[],
+    }),
+    actions: {
+      async getFirebaseToSepet() {
+        const sepetCollection = collection(firestore, 'Sepet');
 
-  }),
-  actions: {
-    async getFirebaseToSepet() {
-      const sepetCollection = collection(firestore, 'Sepet');
+        const querySnapshot = await getDocs(sepetCollection);
 
-      const querySnapshot = await getDocs(sepetCollection);
+        const sepetUrunleri: any[] = [];
 
-      const sepetUrunleri: any[] = [];
-
-      querySnapshot.forEach((doc) => {
-        const data = doc.data();
-        sepetUrunleri.push({
-          urunKodu: data.urunKodu,
-          urunAciklamasi: data.urunAciklamasi,
-          renk: data.renk,
-          beden: data.beden,
-          adet: data.adet,
-          fiyat: data.fiyat,
-          indirimOrani: data.indirimOrani
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          sepetUrunleri.push({
+            urunKodu: data.urunKodu,
+            urunAciklamasi: data.urunAciklamasi,
+            renk: data.renk,
+            beden: data.beden,
+            adet: data.adet,
+            fiyat: data.fiyat,
+            indirimOrani: data.indirimOrani
+          });
         });
-      });
 
-      this.sepetUrunleri = sepetUrunleri;
-    },
+        this.sepetUrunleri = sepetUrunleri;
 
-    async urunEkle({ urunKodu,urunAciklamasi, renk, beden, adet, fiyat, indirimOrani }: { urunKodu: string; urunAciklamasi:string, renk: string; beden: string;  fiyat: number, adet: number;
-      indirimOrani: number;}) {
-      // Kontrol et, eğer ürün zaten varsa adeti bir arttır
-      const existingUrun = this.sepetUrunleri.find((item) => {
-        return item.urunKodu === urunKodu && item.renk === renk && item.beden === beden;
-      });
+      },
 
-      if (existingUrun) {
-        existingUrun.adet += 1;
-      } else {
-        // Eğer ürün yoksa yeni ürünü ekle
-
-        this.sepetUrunleri.push({ urunKodu, urunAciklamasi, renk, beden, adet , fiyat, indirimOrani});
-
-      }
-      await this.pushStoreToFirebase();
-    },
-
-
-    async urunSil({ urunKodu, renk, beden, adet }: { urunKodu: string; renk: string; beden: string; adet: number }) {
-      this.sepetUrunleri = this.sepetUrunleri.filter((item) => {
-        return !(item.urunKodu === urunKodu && item.renk === renk && item.beden === beden && item.adet === adet);
-      });
-    },
-
-    isUrunExists({ urunKodu, renk, beden }: { urunKodu: string; renk: string; beden: string }): boolean {
-      return this.sepetUrunleri.some((item) => {
-        return item.urunKodu === urunKodu && item.renk === renk && item.beden === beden;
-      });
-    },
-
-    async pushStoreToFirebase() {
-      const sepetCollection = collection(firestore, 'Sepet');
-
-      const querySnapshot = await getDocs(sepetCollection);
-
-      querySnapshot.forEach(async (doc) => {
-        await deleteDoc(doc.ref);
-      });
-
-      this.sepetUrunleri.forEach(async (item) => {
-        await addDoc(sepetCollection, {
-          urunKodu: item.urunKodu,
-          urunAciklamasi: item.urunAciklamasi,
-          renk: item.renk,
-          beden: item.beden,
-          adet: item.adet,
-          fiyat: item.fiyat,
-          indirimOrani: item.indirimOrani,
+      async urunEkle({ urunKodu, urunAciklamasi, renk, beden, adet, fiyat, indirimOrani }: {
+        urunKodu: string; urunAciklamasi: string, renk: string; beden: string; fiyat: number, adet: number;
+        indirimOrani: number;
+      }) {
+        // Kontrol et, eğer ürün zaten varsa adeti bir arttır
+        const existingUrun = this.sepetUrunleri.find((item) => {
+          return item.urunKodu === urunKodu && item.renk === renk && item.beden === beden;
         });
-      });
+
+        if (existingUrun) {
+          existingUrun.adet += 1;
+        } else {
+          // Eğer ürün yoksa yeni ürünü ekle
+
+          this.sepetUrunleri.push({ urunKodu, urunAciklamasi, renk, beden, adet, fiyat, indirimOrani });
+
+        }
+        await this.pushStoreToFirebase();
+
+      },
+
+      async urunSil({ urunKodu, renk, beden, adet }: { urunKodu: string; renk: string; beden: string; adet: number }) {
+        this.sepetUrunleri = this.sepetUrunleri.filter((item) => {
+          return !(item.urunKodu === urunKodu && item.renk === renk && item.beden === beden && item.adet === adet);
+        });
+        await this.pushStoreToFirebase();
+      },
+
+
+      isUrunExists({ urunKodu, renk, beden }: { urunKodu: string; renk: string; beden: string }): boolean {
+        return this.sepetUrunleri.some((item) => {
+          return item.urunKodu === urunKodu && item.renk === renk && item.beden === beden;
+        });
+      },
+
+      async pushStoreToFirebase() {
+        const sepetCollection = collection(firestore, 'Sepet');
+
+        const querySnapshot = await getDocs(sepetCollection);
+
+        querySnapshot.forEach(async (doc) => {
+          await deleteDoc(doc.ref);
+        });
+
+        this.sepetUrunleri.forEach(async (item) => {
+          await addDoc(sepetCollection, {
+            urunKodu: item.urunKodu,
+            urunAciklamasi: item.urunAciklamasi,
+            renk: item.renk,
+            beden: item.beden,
+            adet: item.adet,
+            fiyat: item.fiyat,
+            indirimOrani: item.indirimOrani,
+          });
+        });
+      },
     },
-  },
-});
+  });
